dfff8bffd4d6874f95fc2e05942acbd225a83799
MohiraQobilova <139129851+MohiraQobilova@users.noreply.github.com>
2024-09-07 23:37:15 +0400

    Refactoring  according to best practises

--- products/models/misc.py
+++ products/models/misc.py
@@ -1,24 +1,9 @@
-from django.utils import timezone
-
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
+from .product import Product
 
-
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
-
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
-
-    def __str__(self):
-        return self.name
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class Review(models.Model):
@@ -32,12 +17,9 @@ class Review(models.Model):
         return f"{self.product.name} - {self.rating}"
 
 
-
-
 class FlashSale(models.Model):
-
-    product = models.OneToOneField(Product,on_delete=models.CASCADE)
-    discount_percentage = models.PositiveIntegerField()
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    discount_percentage = models.PositiveIntegerField()  # e.g., 20 means 20% off
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
 
